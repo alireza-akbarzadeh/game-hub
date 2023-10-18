d06d9bbf04417ef9099c220b69aba2dfeb47aa81
Alireza Akbarzdeh <aknarzadeh.ali@hasti.co>
2023-10-18 16:56:09 +0330

    get api key

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
-import App from './App'
-import theme from './theme'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import App from "./App";
+import theme from "./theme";
+import "./index.css";
+const API_KEY = "8257b43c498c40428c973400f9b32790";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
