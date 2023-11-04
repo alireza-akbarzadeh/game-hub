9309c5bfd2b8d63667a3b430712feee879a5a5c3
Alireza Akbarzadeh <82927248+alireza-akbarzadeh@users.noreply.github.com>
2023-11-04 23:51:54 +0330

    setup routing

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { GameDetailPage } from "./pages/GameDetailPage";
+import Layout from "./pages/Layout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "games/:id",
+        element: <GameDetailPage />,
+      },
+    ],
+  },
+]);
